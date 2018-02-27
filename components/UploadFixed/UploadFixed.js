23b0e5fc5b533d77a7c7629b5448b42ce4f8b706
DESKTOP-SUP6C2C\Administrator <526800921@qq.com>
2018-02-27 18:02:07 +0800

    右下角悬浮按钮

---
+++ components/UploadFixed/UploadFixed.js
@@ -0,0 +1,30 @@
+// components/UploadFixed/UploadFixed.js
+/*
+ * @Author: Jafish 
+ * @Date: 2018-02-27 17:06:22
+ * @Action: 右下角悬浮上传按钮
+ * @Param: 
+ */
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
