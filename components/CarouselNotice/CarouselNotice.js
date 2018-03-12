8f8ac3083cb84464155d4f4818c8826fe2a65998
DESKTOP-SUP6C2C\Administrator <526800921@qq.com>
2018-03-12 10:43:20 +0800

    更新自定义样式传入方式

--- components/CarouselNotice/CarouselNotice.js
+++ components/CarouselNotice/CarouselNotice.js
@@ -4,8 +4,9 @@
  * @Date: 2018-02-27 15:27:06 
  * @Action: 文字跑马灯，建议文字超出时启用滚动，动画时间8秒
  * @Param: {Boolean} openRoll - 是否开启文字滚动
- * @Param: {String} definedStyle - 自定义最外层style样式，可设置文字继承样式
  * @Param: {String} noticeText - 滚动文字
+ * 
+ * @Param: {String} notice-box-css - 最外层class样式，属性值为class名
  */
 
 Component({
@@ -17,15 +18,14 @@ Component({
       type: Boolean,
       value: true,
     },
-    definedStyle: { // 自定义最外层style样式，字体大小颜色
-      type: String,
-      value: '',
-    },
     noticeText: { // 滚动文字
       type: String,
       value: '非会员用户只有少量查看权限，开通会员获取更多权限吧'
     }
   },
+  externalClasses: [
+    'notice-box-css', // 最外层样式，字体可继承
+  ],
 
   /**
    * 组件的初始数据
